573635d6708e48e2d284affac4f35cc66e5493b1
russpetrus <russpetrus@gmail.com>
2017-05-18 06:13:39 -0400

    Add project files.

---
+++ chatter/Controllers/ChatsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using chatter.Models;
+
+namespace chatter.Controllers
+{
+    public class ChatsController : Controller
+    {
+        private ChatterEntities db = new ChatterEntities();
+
+        // GET: Chats
+        public ActionResult Index()
+        {
+            var chats = db.Chats.Include(c => c.AspNetUser);
+            return View(chats.ToList());
+        }
+
+        public JsonResult TestJson()
+        {
+            string jsonTest = "{ \"firstName\": \"Melanie\", \"lastName\": \"McGee\", \"children\": [{\"firstName\": \"Mira\", \"age\": 13 },{\"firstName\": \"Ethan\", \"age\": null }] }";
+
+                return Json(jsonTest, JsonRequestBehavior.AllowGet);
+            }
+
+
+        // GET: Chats/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chat chat = db.Chats.Find(id);
+            if (chat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chat);
+        }
+
+        // GET: Chats/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Chats/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserId,Message,TimeStamp")] Chat chat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Chats.Add(chat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", chat.UserId);
+            return View(chat);
+        }
+
+        // GET: Chats/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chat chat = db.Chats.Find(id);
+            if (chat == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", chat.UserId);
+            return View(chat);
+        }
+
+        // POST: Chats/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserId,Message,TimeStamp")] Chat chat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(chat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", chat.UserId);
+            return View(chat);
+        }
+
+        // GET: Chats/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chat chat = db.Chats.Find(id);
+            if (chat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chat);
+        }
+
+        // POST: Chats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Chat chat = db.Chats.Find(id);
+            db.Chats.Remove(chat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ chatter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(chatter.Startup))]
+namespace chatter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
